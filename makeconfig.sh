d8e78d95530ddfb575cd9a622d3631d2a2654218
Weihang Lo <b01605002@gmail.com>
2016-07-17 02:15:19 +0800

    add solarized-dark.vimp for vimperator

--- makeconfig.sh
+++ makeconfig.sh
@@ -15,7 +15,7 @@ neovim_init="ln -is $dir/.config/nvim/init.vim $HOME/.vimrc"
 
 
 ## put config/dir your want to sync in this variable
-files=".bashrc .vimrc .vimperatorrc .vimperator/colors/molokai.vimp \
+files=".bashrc .vimrc .vimperatorrc .vimperator/colors/solarized_dark.vimp \
     .tmux.conf .Rprofile .pythonrc.py .inputrc .gitignore .gitconfig \
     .config/nvim/init.vim .xvimrc"
 
