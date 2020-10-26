c812981715bf9c795756693ecc8c0049a2fd0923
Weihang Lo <me@weihanglo.tw>
2020-10-26 14:10:31 +0800

    [fish] separate login/interative shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,78 +6,58 @@
 #              Oct. 2020               #
 #--------------------------------------#
 
-if set -q config_fish_loaded
-    # config.fish has already loaded.
-    # source starship again before failing fast
+# ------------------------------------------------------------------------------
+# Run for login shell.
+# ------------------------------------------------------------------------------
+
+if status is-login
+    # Default shell to fish
+    set -gx SHELL fish
+    # LESS pager. More power.
+    set -gx LESS isFMRX
+    # EDITOR and VISUAL
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim 
+
+    # Aditional PATHs
+    set -agx PATH $HOME/.local/bin
+    # FZF
+    set -gx FZF_DEFAULT_COMMAND "rg --files --smart-case"
+    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+    set -pgx PATH $HOME/.fzf/bin
+    # Ruby
+    set -gx GEM_HOME $HOME/.gem
+    set -pgx PATH $GEM_HOME/bin
+    # Golang
+    set -gx GOPATH $HOME/go
+    set -pgx PATH $GOPATH/bin
+    # Python
+    set -gx PYENV_ROOT $HOME/.pyenv
+    set -gx PIPENV_PYTHON $PYENV_ROOT/shims/python
+    set -pgx PATH $PYENV_ROOT/bin
+    # Rust
+    set -pgx PATH $HOME/.cargo/bin
+
+    # Load `fnm` once and for all
+    fnm env | source
+    # Load `pyenv` once and for all
+    pyenv init - | source
+end
+
+# ------------------------------------------------------------------------------
+# Load only for interative shell.
+# ------------------------------------------------------------------------------
+
+if status is-interactive
+    alias ls="exa"
+    alias ll="exa -lhgF --git"
+    alias ports="lsof -PiTCP -sTCP:LISTEN"
+    alias tree="exa -TF --group-directories-first"
+    alias cat="bat"
+    # Customizable prompt
     starship init fish | source
-    exit
-end
-set -gx config_fish_loaded
-
-#---------------------------------------
-# User specific aliases and functions
-#---------------------------------------
-function ls -d 'Alias ls to exa.'
-    exa $argv
 end
 
-function ll -d 'Alias ll to exa for `ls -l` with git status.'
-    exa -lhgF --git $argv
-end
-
-function ports -d 'List all listening TCP ports. Add sudo if needed.'
-    lsof -PiTCP -sTCP:LISTEN $argv
-end
-
-function tree -d 'Alias tree to exa'
-    exa -TF --group-directories-first $argv
-end
-
-function cat -d 'Alias cat to bat'
-    bat $argv
-end
-
-#---------------------------------------
-# Environment variables and configs
-#---------------------------------------
-# Default shell to fish
-set -gx SHELL fish
-
-# LESS pager. More power.
-set -gx LESS "isFMRX"
-
-# Aditional PATHs
-set -gx fish_user_paths "$HOME/.local/bin" $fish_user_paths
-
-# EDITOR and VISUAL
-set -gx VISUAL nvim 
-set -gx EDITOR nvim
-
-# Ruby GEM_PATH
-set -gx GEM_HOME "$HOME/.gem"
-set -gx fish_user_paths "$GEM_HOME/bin" $fish_user_paths
-
-# Golang environment
-set -gx GOPATH "$HOME/.go"
-set -gx fish_user_paths "$GOPATH/bin" $fish_user_paths
-
-# Node.js
-fnm env | source
-
-# Python
-set -gx PYENV_ROOT "$HOME/.pyenv"
-set -gx fish_user_paths "$PYENV_ROOT/bin" "$fish_user_paths"
-set -gx PIPENV_PYTHON "$PYENV_ROOT/shims/python"
-pyenv init - | source
-
-
-# RUST
-set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
-
-# Just launch the starship!!! (https://starship.rs/)
-starship init fish | source
-
-# FZF default configs
-set -gx FZF_DEFAULT_COMMAND "rg --files --smart-case"
-set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -gx fish_user_paths $fish_user_paths "$HOME/.fzf/bin"
+# ------------------------------------------------------------------------------
+# Commands that always run for all sessions go below.
+# ------------------------------------------------------------------------------
