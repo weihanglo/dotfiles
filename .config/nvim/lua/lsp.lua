b656b68481e90b1f34b64e6372cd89dd1d0c4895
Weihang Lo <me@weihanglo.tw>
2021-03-10 14:40:34 +0800

    [nvim] update lus lsp config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -118,9 +118,40 @@ end
 -- `git clone https://github.com/sumneko/lua-language-server` and build!
 --
 -- Ref: https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/sumneko_lua.lua#L25-L65
 M.sumneko_lua_setup = function()
-  lspconfig.sumneko_lua.setup{
+  local system_name
+  if vim.fn.has("mac") == 1 then
+    system_name = "macOS"
+  elseif vim.fn.has("unix") == 1 then
+    system_name = "Linux"
+  elseif vim.fn.has('win32') == 1 then
+    system_name = "Windows"
+  else
+    print("Unsupported system for sumneko")
+  end
+  local sumneko_root_path = vim.fn.expand('~')..'/wd/quick-dirty/lua-language-server'
+  local sumneko_binary = sumneko_root_path..'/bin/'..system_name..'/lua-language-server'
+  lspconfig.sumneko_lua.setup {
     on_attach = on_attach,
+    cmd = {sumneko_binary, '-E', sumneko_root_path..'/main.lua'};
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = vim.split(package.path, ';'),
+        },
+        diagnostics = {
+          globals = {'vim'},
+        },
+        workspace = {
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+      },
+    },
   }
 end
 
