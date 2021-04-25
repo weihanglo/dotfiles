fca32a339e263fe6d167c53fbd6a04360b535b77
Weihang Lo <me@weihanglo.tw>
2021-04-25 22:55:33 +0800

    [nvim] refactor lua script

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -7,16 +7,15 @@ local on_attach = function(client, bufnr)
   -- This will setup with buffers attached with LSP clients.
   require'compe'.setup {
     source = {
-      path = true;
-      buffer = true;
-      tags = true;
-      spell = true;
-      calc = true;
-      omni = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      vsnip = true;
-      treesitter = true;
+      path = true,
+      buffer = true,
+      tags = true,
+      spell = true,
+      calc = true,
+      emoji = true,
+      nvim_lsp = true,
+      nvim_lua = true,
+      vsnip = true,
     };
   }
 
