de6c44cd0c0e6f8400316e620d303ac8bff852c7
Weihang Lo <weihang.lo@suse.com>
2021-07-16 09:54:29 +0800

    [nvim] colorcolume is back (using gruvbox soft variant)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,11 +4,11 @@ local map = vim.api.nvim_set_keymap
 
 --- sainnhe/gruvbox-material
 local function gruvbox_material_setup()
-  vim.g.gruvbox_material_background = 'hard'
+  vim.g.gruvbox_material_background = 'soft'
   vim.g.gruvbox_material_better_performance = 1
   vim.g.gruvbox_material_diagnostic_line_highlight = 1
   vim.g.gruvbox_material_diagnostic_text_highlight = 1
-  vim.g.gruvbox_material_enable_bold = 1
+  vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
   vim.g.gruvbox_material_enable_italic = 1
   vim.g.gruvbox_material_transparent_background = 1
   vim.cmd [[colorscheme gruvbox-material]]
