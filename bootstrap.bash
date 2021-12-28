91512906fc48e97e57d89bb6e032ddb31ce28367
Weihang Lo <me@weihanglo.tw>
2021-12-29 01:38:59 +0800

    [boostrap] refine the script

--- bootstrap.bash
+++ bootstrap.bash
@@ -5,16 +5,11 @@
 #              Dec. 2021               #
 #--------------------------------------#
 
-# Define source and backup directory
-
-dir=$HOME/.dotfiles
-origdir=$HOME/.dotfiles.orig
-
 # --------------------------------------
 # Put config/dir to sync in this variable
 # --------------------------------------
 
-files=(
+files_to_sync=(
     .bashrc
     .cargo/config.toml
     .config/alacritty/alacritty.yml
@@ -34,73 +29,85 @@ files=(
     .tmux.conf
 )
 
+cargo_crates=(
+  bat
+  cargo-update
+  exa
+  fd-find
+  git-delta
+  gitui
+  mdbook
+  ripgrep
+  starship
+  tealdeer
+  tokei
+  watchexec-cli
+  zoxide
+)
+
+python_packages=(
+  jupyter
+  pipenv
+  poetry
+  python-lsp-server[all]
+)
+
 # --------------------------------------
 # Put custom installaltion commands here
 # --------------------------------------
 
-install_cargo_binaries() {
-  crates=(
-    bat
-    cargo-update
-    exa
-    fd-find
-    git-delta
-    gitui
-    mdbook
-    ripgrep
-    starship
-    tealdeer
-    tokei
-    watchexec-cli
-    zoxide
-  )
+sync_config_files () {
+  # Define source and backup directory
+  dir=$HOME/.dotfiles
+  origdir=$HOME/.dotfiles.orig
 
-  cargo install ${crates[@]}
+  echo -n "Creating $origdir for backup ..."
+  mkdir -p $origdir
+  echo "done"
+
+  echo -n "cd to $dir ..."
+  cd $dir
+  echo "done"
+
+  # Symlink files
+  for file in ${files_to_sync[@]}; do
+      echo "$file"
+      echo -e "\tMoving $file to $origdir/$file"
+      if [[ -z "$file" ]]; then
+          echo -e "No file $file found"
+          return
+      fi
+      mkdir -p "$HOME/$(dirname $file)"
+      mkdir -p "$origdir/$(dirname $file)"
+      mv "$HOME/$file" "$origdir/$file"
+      echo -e "\tSymlinking to $file in $dir"
+      ln -is "$dir/$file" "$HOME/$file"
+  done
 }
 
 install_tmux_package_manager() {
-    if [[ ! -d "$HOME/.tmux/plugins/tpm" ]]; then
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tpm_dir="$HOME/.tmux/plugins/tpm"
+    if [[ ! -d "$tpm_dir" ]]; then
+        git clone https://github.com/tmux-plugins/tpm "$tpm_dir"
     else
         echo 'TMUX Package Manager has already be installed.'
     fi
 }
 
-# --------------------------------------
-# Start symlink files
-# --------------------------------------
+install_cargo_binaries() {
+  cargo install ${cargo_crates[@]}
+}
 
-echo -n "Creating $origdir for backup ..."
-mkdir -p $origdir
-echo "done"
-
-echo -n "cd to $dir ..."
-cd $dir
-echo "done"
-
-# Symlink files
-for file in ${files[@]}; do
-    echo "$file"
-    echo -e "\tMoving $file to $origdir/$file"
-    if [[ -z "$file" ]]; then
-        echo -e "No file $file found"
-        return
-    fi
-    mkdir -p "$HOME/$(dirname $file)"
-    mkdir -p "$origdir/$(dirname $file)"
-    mv "$HOME/$file" "$origdir/$file"
-    echo -e "\tSymlinking to $file in $dir"
-    ln -is "$dir/$file" "$HOME/$file"
-done
-
-# --------------------------------------
-# Call pre-defined custom commmands here
-# --------------------------------------
+install_python_binaries() {
+  python3 -m pip install -U pipx pip
+  python3 -m pipx ensurepath
+  echo ${python_packages[@]} | xargs -n1 python3 -m pipx install --include-deps
+}
 
 # confirm helper function running before installation
 confirm () {
     echo
-    read -p "${3:-}Confirm $1? [y/N]" response
+    read -p "${3:-}$1? [y/N]" response
     case $response in
         [yY][eE][sS]|[yY])
             $2
@@ -112,8 +119,28 @@ confirm () {
     echo
 }
 
+# ----------------------------------------
+# Main function that calls other functions
+# ----------------------------------------
 
-# confirm installation
-
-confirm "install tpm (TMUX Package Manager)" install_tmux_package_manager
-confirm "install all goods from cargo" install_cargo_binaries
+if [[ -n "$1" ]]; then
+  case "$1" in
+    sync)
+      sync_config_files
+      ;;
+    tpm)
+      install_tmux_package_manager
+      ;;
+    cargo|rust)
+      install_cargo_binaries
+      ;;
+    python|py)
+      install_python_binaries
+      ;;
+  esac
+else
+  confirm "Synchronize all config files" sync_config_files
+  confirm "Install tpm (TMUX Package Manager)" install_tmux_package_manager
+  confirm "Install all goods from cargo" install_cargo_binaries
+  confirm "Install useful python binaries" install_python_binaries
+fi
