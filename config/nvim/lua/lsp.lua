3c140512ed6cc738680d58606cc1cac7d2d670f3
Weihang Lo <me@weihanglo.tw>
2025-03-04 15:24:20 -0500

    [nvim] remove workaround as it is patched in 0.10.3

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -81,19 +81,6 @@ local function on_attach(client, bufnr)
       false
     )
   end
-
-  -- Workaround for https://github.com/neovim/neovim/issues/30985
-  -- Has already been fixed via https://github.com/neovim/neovim/pull/31345
-  -- and will be released in 0.11
-  for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
-    local default_diagnostic_handler = vim.lsp.handlers[method]
-    vim.lsp.handlers[method] = function(err, result, context, config)
-      if err ~= nil and err.code == -32802 then
-        return
-      end
-      return default_diagnostic_handler(err, result, context, config)
-    end
-  end
 end
 
 --- Rust Analyzer setup.
