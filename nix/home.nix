9cbfe0ef6dc8530538ad1de277d8ec3378c58012
Weihang Lo <me@weihanglo.tw>
2026-04-24 00:31:16 -0400

    [nix] home manager state bump

--- nix/home.nix
+++ nix/home.nix
@@ -22,7 +22,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "26.05"; # Please read the comment before changing.
 
   home.shellAliases = {
     cat = "bat";
