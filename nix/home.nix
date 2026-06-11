78706e13480c3db4538e6d3d98f40bbd296595f2
Weihang Lo <me@weihanglo.tw>
2026-06-11 07:19:15 -0400

    [jj] config

--- nix/home.nix
+++ nix/home.nix
@@ -142,7 +142,70 @@
 
   programs.jq.enable = true;
 
-  programs.jujutsu.enable = true;
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      aliases = {
+        c = [ "commit" ];
+        ci = [
+          "commit"
+          "--interactive"
+        ];
+        difft = [
+          "diff"
+          "--tool"
+          "difft"
+        ];
+        n = [ "new" ];
+        pull = [
+          "git"
+          "fetch"
+          "--all-remotes"
+        ];
+        push = [
+          "git"
+          "push"
+        ];
+        r = [ "rebase" ];
+        s = [ "squash" ];
+        si = [
+          "squash"
+          "--interactive"
+        ];
+      };
+      git = {
+        sign-on-push = true;
+      };
+      merge-tools = {
+        delta = {
+          diff-expected-exit-codes = [
+            0
+            1
+          ];
+        };
+        difft = {
+          diff-args = [
+            "--color=always"
+            "$left"
+            "$right"
+          ];
+        };
+      };
+      signing = {
+        behavior = "drop";
+        backend = "gpg";
+        key = "D7DBF189825E82E7";
+      };
+      ui = {
+        pager = "delta";
+        diff-formatter = ":git";
+      };
+      user = {
+        name = "Weihang Lo";
+        email = "me@weihanglo.tw";
+      };
+    };
+  };
 
   programs.neovim = {
     enable = true;
